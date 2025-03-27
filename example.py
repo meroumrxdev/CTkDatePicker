b9172cae699f226727268e7039102d19159103b8
meroumrxdev <nanohell807@gmail.com>
2025-03-26 22:39:21 -0700

    علبالك خدمة هايلة المهم جرب نورمالموا كلش يخدم

--- example.py
+++ example.py
@@ -1,16 +1,21 @@
+import tkinter as tk
 import customtkinter as ctk
-from CTkDatePicker import *
+from CTkDatePicker import CTkDatePicker
+
+def main():
+    root = ctk.CTk()
+    root.geometry("400x300")
+    
+    date_picker = CTkDatePicker(root)
+    date_picker.pack(pady=20)
+
+    def print_date():
+        print(f"Selected Date: {date_picker.get_date()}")
+
+    btn = ctk.CTkButton(root, text="Print Date", command=print_date)
+    btn.pack(pady=20)
+    
+    root.mainloop()
 
 if __name__ == "__main__":
-    root = ctk.CTk()
-    root.geometry("450x200")
-    root.title("CTkDatePicker Example")
-
-    date_picker = CTkDatePicker(root)
-    date_picker.pack(padx=20, pady=20)
-    date_picker.set_allow_manual_input(False)
-    
-    # get value
-    # date.picker.get_date()
-
-    root.mainloop()
+    main()
